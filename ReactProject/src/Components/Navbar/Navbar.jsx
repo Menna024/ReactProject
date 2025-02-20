6f98effe08e175aa3d6e6736be779446f427240c
Menna024 <mennaayman0246@gmail.com>
2025-02-21 00:27:36 +0200

    Refactor Navbar component to enhance token handling and improve styling; add useEffect for token logging

--- ReactProject/src/Components/Navbar/Navbar.jsx
+++ ReactProject/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import './Navbar.css';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { TokenContext } from '../../Context/TokenContext';
-import { useContext } from 'react';
+import { useEffect, useContext } from 'react';
 
 const Navbar = () => {
     const token = useContext(TokenContext);
@@ -15,21 +15,26 @@ const Navbar = () => {
         navigate('/login');
     }
 
+    useEffect(() => {
+        console.log('token:', token);
+        console.log('token.Token:', localStorage.getItem('token'));
+    }, [token]);
+
     return (
         <div className="navbar">
             <h1 className="site-name">Fresh Cart</h1>
-            {token && console.log('token:', token)}
-            {token.Token &&
-                <div className="nav-links">
-                    <NavLink to="/home" className="nav-link">Home</NavLink>
-                    <NavLink to="/products" className="nav-link">Products</NavLink>
-                    <NavLink to="/cart" className="nav-link">Cart</NavLink>
-                    <NavLink to="/wishlist" className="nav-link">WishList</NavLink>
-                    <NavLink to="/categories" className="nav-link">Categories</NavLink>
-                    <NavLink to="/brands" className="nav-link">Brands</NavLink>
+            {(token ||  localStorage.getItem('token'))  && console.log('token:', token)}
+            {(token.Token ||  localStorage.getItem('token'))&&
+                <div className="nav-link-items">
+                    <NavLink to="/home" className="nav-link-item">Home</NavLink>
+                    <NavLink to="/products" className="nav-link-item">Products</NavLink>
+                    <NavLink to="/cart" className="nav-link-item">Cart</NavLink>
+                    <NavLink to="/wishlist" className="nav-link-item">WishList</NavLink>
+                    <NavLink to="/categories" className="nav-link-item">Categories</NavLink>
+                    <NavLink to="/brands" className="nav-link-item">Brands</NavLink>
                 </div>
             }
-            {!token.Token &&
+            {!(token.Token ||  localStorage.getItem('token')) &&
             <div className='sign-btns' >
                     <button className="signup-btn" onClick={goToRegister}>Register</button>
                     <button className="login-btn" onClick={goToLogin}>Log In</button>
@@ -37,7 +42,7 @@ const Navbar = () => {
             }
 
             {
-                token.Token &&
+                (token.Token ||  localStorage.getItem('token')) &&
                 <div className='sign-btns'>
                     <button className="logout-btn" onClick={() => {
                         localStorage.removeItem('user');
